273b5084a3f47482faf644c8876d66401be38132
samuelmalik <samuelmalik9@gmail.com>
2022-10-30 21:17:36 +0100

    add new class Book

---
+++ Open-Lab-10.00/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._00
+{
+    internal class Book
+    {
+        public string title;
+        private int pages;
+    }
+}
